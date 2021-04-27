32ad6fd48cc4c76312f14fb61e23e7f3316fec7a
Martin Lopez <martin.lopez@naranjax.com>
2021-04-27 13:07:46 -0300

    Change filter State

--- src/App.js
+++ src/App.js
@@ -11,14 +11,14 @@ const initialTask = [
     name: 'Diseñar banner', 
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
     priority: 'Baja', 
-    stateTask: 'En proceso' 
+    stateTask: 'En progreso' 
   },
   { 
     id: 2, 
     name: 'Desarrollar tooltip', 
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 
     priority: 'Media', 
-    stateTask: 'En proceso' 
+    stateTask: 'En progreso' 
   },
 ];
 
